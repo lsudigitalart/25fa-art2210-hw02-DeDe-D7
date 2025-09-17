11af6a3f2d0adf48b02c891710d8b8b853f9a977
DeDe-D7 <ddunc22@lsu.edu>
2025-09-17 01:32:46 -0500

    finished

--- sketch.js
+++ sketch.js
@@ -1,7 +1,130 @@
 function setup() {
-  createCanvas(400, 269);
+  createCanvas(950, 642);
+  background(3,3,3);
+
 }
 
 function draw() {
+stroke (5,4,2)
+fill (213,170,5);
+ellipse (50,440, 600,700,)
+stroke (187,183,176);
+stroke (1,7,57);
+fill (1,7,57);
+ellipse (790,531,490,1000);
+ellipse (700,545,420,900);
+ellipse (650,571,490,900);
+
+fill (5,4,1)
+circle (837,221,400,400)
+
+rect (850,580,5,2)
+rect (781,594,2,2)
+rect (293,413,6,4)
+
+
+fill (105,0,2);
+stroke (105,0,2);
+rect (0,0,1000,145,0);
+stroke (187,183,176);
+fill (187,183,176);
+ellipse (260,699,390);
+stroke (187,183,176);
+ellipse(27,580,780,227);
+
+stroke (5,4,1)
+noFill ();
+strokeWeight (12);
+bezier (2,474,340,430,437,570,447,638)
+
+stroke (5,4,1)
+noFill ();
+strokeWeight (32);
+bezier (5,420,42,428,63,435,127,462)
+
+stroke (5,4,1)
+noFill ();
+strokeWeight (12);
+bezier (5,420,42,428,63,435,127,462)
+
+stroke (5,4,1)
+noFill ();
+strokeWeight (12);
+bezier (1,301,54,334,118,407,155,467)
+
+stroke (5,4,1)
+noFill ();
+strokeWeight (10);
+bezier (0,254,147,242,234,349,344,493)
+
+stroke (5,4,1)
+noFill ();
+strokeWeight (13);
+bezier (9,185,295,262,235,418,365,505)
+
+stroke (5,4,1)
+noFill ();
+strokeWeight (13)
+bezier (0,220,121,247,216,286,463,659)
+
+stroke (5,4,1)
+noFill ();
+strokeWeight (13)
+bezier (0,170,218,116,280,314,400,500)
+
+stroke (5,4,1)
+noFill ();
+strokeWeight (12);
+bezier (0,450,42,428,63,435,127,462)
+
+
+
+stroke (5,4,2)
+strokeWeight (1)
+fill (5,4,2)
+ellipse (658,148,2000,9)
+ellipse (658,129,2000,9)
+ellipse (658,75,2000,15);
+
+stroke (5,4,2)
+strokeWeight (1)
+fill (5,4,2)
+ellipse (758,548,500,10)
+
+fill (5,4,1)
+
+ellipse (858,310,380,200)
+ellipse (858,410,480,200)
+ellipse (858,450,380,200)
+ellipse (858,580,380,300)
+
+ellipse (558,650,610,15)
+ellipse (658,444,180,15)
+ellipse (658,454,280,15)
+ellipse (658,474,280,15)
+ellipse (658,494,180,15)
+ellipse (658,344,250,15)
+ellipse (658,354,270,15)
+ellipse (658,374,190,15)
+ellipse (658,584,285,45)
+ellipse (658,410,300,15)
+ellipse (658,210,290,15)
+ellipse (658,310,260,15)
+ellipse (658,240,290,15)
+ellipse (658,235,360,15)
+ellipse (658,260,180,15)
+
+
+ellipse (658,610,180,15)
+ellipse (658,630,180,15)
+ellipse (658,595,180,15)
+ellipse (658,570,180,15)
+ellipse (658,575,180,15)
+ellipse (658,520,180,15)
+ellipse (658,540,180,15)
+
+
+
+
 
 }
